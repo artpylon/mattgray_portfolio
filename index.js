fee0da341f3167a953af3d1bcdbb8cf2e9fdd5d0
artpylon <matthewgray88@gmail.com>
2017-07-04 21:37:28 -0400

    commented index.js

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 'use strict'
-$(() => {
-
-})
-
-const addHandlers = () => {
-  $('.carousel').carousel({
-    interval: 2000
-  })
-}
-
-module.exports = {
-  addHandlers
-}
+// $(() => {
+//
+// })
+//
+// const addHandlers = () => {
+//   $('.carousel').carousel({
+//     interval: 2000
+//   })
+// }
+//
+// module.exports = {
+//   addHandlers
+// }
