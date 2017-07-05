6f669d72deee9f03b6293ee35b14674a493a38a7
artpylon <matthewgray88@gmail.com>
2017-07-04 21:43:44 -0400

    Remove body styling

--- index.js
+++ index.js
@@ -1,14 +1,1 @@
 'use strict'
-// $(() => {
-//
-// })
-//
-// const addHandlers = () => {
-//   $('.carousel').carousel({
-//     interval: 2000
-//   })
-// }
-//
-// module.exports = {
-//   addHandlers
-// }
